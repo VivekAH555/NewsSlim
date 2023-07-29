7b2362ad82a46d739e3256f75e8cb370705a876c
VivekAH555 <140909243+VivekAH555@users.noreply.github.com>
2023-07-30 00:11:34 +0530

    Intial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Footer = (props) => {
+  const [backgroundColor, setBackgroundColor] = useState(
+    props.mode === "light" ? "#eae9e9" : "#444444"
+  );
+  const [textColor, setTextColor] = useState(
+    props.mode === "light" ? "black" : "#eae9e9"
+  );
+
+  return (
+    <div>
+      <div className="footer-container" style={{ backgroundColor }}>
+        <p className="center" style={{ color: textColor }}>
+          Developed by Vivek Hegde
+        </p>
+        <div className="footer">
+          <a
+            href="https://github.com/datta1054/News-Slim"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
